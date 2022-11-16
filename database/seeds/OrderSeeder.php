4cf6c8f48102296efd562cc641efe86862e2b5f9
giusscos <giu.cosenza08@gmail.com>
2022-11-16 18:59:25 +0100

    add order model-all

---
+++ database/seeds/OrderSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+use App\Order;
+use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+
+class OrderSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(Faker $faker)
+    {
+
+        $status = ['Completato', 'In lavorazione', 'In consegna', 'In elaborazione'];
+
+        for ($i = 0; $i < 50; $i++) {
+            $order = new Order();
+            $order->status = $faker->randomElement($status);
+            $order->total = floatval($faker->numerify('###.##'));
+            $order->name_client = $faker->firstName();
+            $order->surname_client = $faker->lastName();
+            $order->address_client = $faker->address();
+            $order->phone_client = $faker->numerify('+39 ### ### ####');
+            $order->email_client = $faker->email();
+
+            $order->save();
+        }
+    }
+}
