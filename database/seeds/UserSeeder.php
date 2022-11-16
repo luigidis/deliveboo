074518ccd53b13c3bd070eb23b8fb0fda2508705
LuigiDiS <luigi.distefano69@gmail.com>
2022-11-16 16:41:39 +0100

    auth

---
+++ database/seeds/UserSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+use App\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::create([
+            'name' => 'Admin',
+            'email' => 'admin.01@gmail.com',
+            'password' => Hash::make('pippo123'),
+        ]);
+    }
+}
