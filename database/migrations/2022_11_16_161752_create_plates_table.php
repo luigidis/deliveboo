dd76636005b65310d2b676c8e4a22b6914470b66
FabioLongoni <longoni.fabio90@gmail.com>
2022-11-16 18:58:12 +0100

    add migration and seeder Plates

---
+++ database/migrations/2022_11_16_161752_create_plates_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePlatesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('plates', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('img')->nullable();
+            $table->text('description');
+            $table->smallInteger('price');
+            $table->boolean('visible');
+            $table->string('slug')->unique();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('plates');
+    }
+}
