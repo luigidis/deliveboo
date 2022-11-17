b1a36f50989b233ac8a2adfe8e2825fbe4852a37
Francesco Pieraccini <pieraccini.francesco@gmail.com>
2022-11-17 16:31:32 +0100

    update databaseseeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -17,6 +17,7 @@ class DatabaseSeeder extends Seeder
             CategorySeeder::class,
             PlateSeeder::class,
             OrderSeeder::class,
+            OrderPlateSeeder::class,
         ]);
     }
 }
