b457aefe07e09d7923c6517fd804572b43abd453
LuigiDiS <luigi.distefano69@gmail.com>
2022-11-23 10:49:59 +0100

    register form fluid

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,173 +1,198 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Register') }}</div>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">{{ __('Register') }}</div>
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('register') }}" enctype="multipart/form-data">
-                        @csrf
+                    <div class="card-body">
+                        <form method="POST" action="{{ route('register') }}" enctype="multipart/form-data">
+                            @csrf
 
-                        <div class="form-group row">
-                            <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Name') }}</label>
+                            <div class="form-group row">
+                                <label for="name"
+                                    class="col-md-4 col-form-label text-md-right">{{ __('Name') }}</label>
 
-                            <div class="col-md-6">
-                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
+                                <div class="col-md-6">
+                                    <input id="name" type="text"
+                                        class="form-control @error('name') is-invalid @enderror" name="name"
+                                        value="{{ old('name') }}" required autocomplete="name" autofocus>
 
-                                @error('name')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="restaurant_name" class="col-md-4 col-form-label text-md-right">Nome Attività</label>
-
-                            <div class="col-md-6">
-                                <input id="restaurant_name" type="string" name="restaurant_name" class="form-control
-                                @error('restaurant_name') is-invalid @enderror" required>
-                                
-                                @error('restaurant_name')
-                                <span class="invalid-feedback" role="alert">
-                                    <strong>{{ $message }}</strong>
-                                </span>
-                                @enderror
-                                
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="address" class="col-md-4 col-form-label text-md-right">Indirizzo</label>
-
-                            <div class="col-md-6">
-                                <input id="address" type="string" name="address" class="form-control @error('address') is-invalid @enderror" required>
-                                
-                                @error('address')
-                                <span class="invalid-feedback" role="alert">
-                                    <strong>{{ $message }}</strong>
-                                </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="phone" class="col-md-4 col-form-label text-md-right">Numero di telefono</label>
-
-                            <div class="col-md-6">
-                                <input id="phone" type="tel" name="phone" class="form-control @error('phone') is-invalid @enderror" required>
-                                
-                                @error('phone')
-                                <span class="invalid-feedback" role="alert">
-                                    <strong>{{ $message }}</strong>
-                                </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="p_iva" class="col-md-4 col-form-label text-md-right">Partita Iva</label>
-
-                            <div class="col-md-6">
-                                <input id="p_iva" type="string" name="p_iva" class="form-control @error('p_iva') is-invalid @enderror" required>
-                                
-                                @error('p_iva')
-                                <span class="invalid-feedback" role="alert">
-                                    <strong>{{ $message }}</strong>
-                                </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <?php
-                        use App\Category;
-                        $categories = Category::orderBy('name', 'asc')->get();
-                        ?>
-
-                        <div class="form-group row">
-                            <label class="col-md-4 col-form-label text-md-right" for="category">Categorie:</label>
-                  
-                            <div class="col-md-6">
-                                <div class="@error('categories') is-invalid @enderror">
-                                    
-                                    @foreach($categories as $key => $category)
-                                    <div class="form-check form-check-inline">
-                                  <input class="form-check-input" name="categories[]" @if( in_array($category->id, old('categories', []) ) ) checked @endif type="checkbox" id="category-{{$category->id}}" value="{{ $category->id }}">
-                                  <label class="form-check-label" for="category-{{$category->id}}">{{ $category->name }}</label>
+                                    @error('name')
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                    @enderror
                                 </div>
-                                @endforeach
-                                
                             </div>
-                            @error('categories')
-                            <span class="invalid-feedback" role="alert">
-                                <strong>{{ $message }}</strong>
-                            </span>
-                            @enderror
-                        </div>
 
-                        <div class="form-group">
-                            <label for="image" class="font-weight-bold">Scegli immagine</label>
-                            <input type="file" class="form-control-file @error('image') is-invalid @enderror" id="image"
-                                name="image">
-                           
-                            @error('image')
-                                <div id="image" class="invalid-feedback">
-                                    {{ $message }}
+                            <div class="form-group row">
+                                <label for="email"
+                                    class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+
+                                <div class="col-md-6">
+                                    <input id="email" type="email"
+                                        class="form-control @error('email') is-invalid @enderror" name="email"
+                                        value="{{ old('email') }}" required autocomplete="email">
+
+                                    @error('email')
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                    @enderror
                                 </div>
-                            @enderror
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Register') }}
-                                </button>
                             </div>
-                        </div>
-                    </form>
+
+                            <div class="form-group row">
+                                <label for="password"
+                                    class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
+
+                                <div class="col-md-6">
+                                    <input id="password" type="password"
+                                        class="form-control @error('password') is-invalid @enderror" name="password"
+                                        required autocomplete="new-password">
+
+                                    @error('password')
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                    @enderror
+                                </div>
+                            </div>
+
+                            <div class="form-group row">
+                                <label for="password-confirm"
+                                    class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
+
+                                <div class="col-md-6">
+                                    <input id="password-confirm" type="password" class="form-control"
+                                        name="password_confirmation" required autocomplete="new-password">
+                                </div>
+                            </div>
+
+                            <div class="form-group row">
+                                <label for="restaurant_name" class="col-md-4 col-form-label text-md-right">Nome
+                                    Attività</label>
+
+                                <div class="col-md-6">
+                                    <input id="restaurant_name" type="string" name="restaurant_name"
+                                        class="form-control
+                                @error('restaurant_name') is-invalid @enderror"
+                                        required>
+
+                                    @error('restaurant_name')
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                    @enderror
+
+                                </div>
+                            </div>
+
+                            <div class="form-group row">
+                                <label for="address" class="col-md-4 col-form-label text-md-right">Indirizzo</label>
+
+                                <div class="col-md-6">
+                                    <input id="address" type="string" name="address"
+                                        class="form-control @error('address') is-invalid @enderror" required>
+
+                                    @error('address')
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                    @enderror
+                                </div>
+                            </div>
+
+                            <div class="form-group row">
+                                <label for="phone" class="col-md-4 col-form-label text-md-right">Numero di
+                                    telefono</label>
+
+                                <div class="col-md-6">
+                                    <input id="phone" type="tel" name="phone"
+                                        class="form-control @error('phone') is-invalid @enderror" required>
+
+                                    @error('phone')
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                    @enderror
+                                </div>
+                            </div>
+
+                            <div class="form-group row">
+                                <label for="p_iva" class="col-md-4 col-form-label text-md-right">Partita Iva</label>
+
+                                <div class="col-md-6">
+                                    <input id="p_iva" type="string" name="p_iva"
+                                        class="form-control @error('p_iva') is-invalid @enderror" required>
+
+                                    @error('p_iva')
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                    @enderror
+                                </div>
+                            </div>
+
+                            <?php
+                            use App\Category;
+                            $categories = Category::orderBy('name', 'asc')->get();
+                            ?>
+
+                            <div class="form-group row">
+                                <label class="col-md-4 col-form-label text-md-right" for="category">Categorie:</label>
+
+                                <div class="col-md-6">
+                                    <div class="@error('categories') is-invalid @enderror">
+
+                                        @foreach ($categories as $key => $category)
+                                            <div class="form-check form-check-inline">
+                                                <input class="form-check-input" name="categories[]"
+                                                    @if (in_array($category->id, old('categories', []))) checked @endif type="checkbox"
+                                                    id="category-{{ $category->id }}" value="{{ $category->id }}">
+                                                <label class="form-check-label"
+                                                    for="category-{{ $category->id }}">{{ $category->name }}</label>
+                                            </div>
+                                        @endforeach
+
+                                    </div>
+                                    @error('categories')
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                    @enderror
+                                </div>
+                            </div>
+
+                            <div class="form-group row">
+                                <label for="image"
+                                    class="col-md-4 col-form-label text-md-right">Scegli
+                                    immagine</label>
+                                <div class="col-md-6">
+                                    <input type="file" class="form-control-file @error('image') is-invalid @enderror"
+                                        id="image" name="image">
+                                </div>
+                                @error('image')
+                                    <div id="image" class="invalid-feedback">
+                                        {{ $message }}
+                                    </div>
+                                @enderror
+                            </div>
+
+                            <div class="form-group row mb-0">
+                                <div class="col-md-6 offset-md-4">
+                                    <button type="submit" class="btn btn-primary">
+                                        {{ __('Register') }}
+                                    </button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
 @endsection
