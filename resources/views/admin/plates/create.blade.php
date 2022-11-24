657c12ca49aa7b308bb2d73ce7db85c1b711d143
LuigiDiS <luigi.distefano69@gmail.com>
2022-11-24 09:48:32 +0100

    last push

--- resources/views/admin/plates/create.blade.php
+++ resources/views/admin/plates/create.blade.php
@@ -1,208 +1,223 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-  <div class="row justify-content-between">
-    <div class="col-8">
-      <h1 class="title">Aggiungi un nuovo piatto al menù</h1>
+    <div class="container">
+        <div class="row justify-content-between">
+            <div class="col-8">
+                <h1 class="title">Aggiungi un nuovo piatto al menù</h1>
+            </div>
+            <div class="btn-y">
+                <a href="{{ route('admin.plates.index') }}"><button type="button" class="btn btn-warning btn-lg">Torna
+                        indietro</button></a>
+            </div>
+        </div>
     </div>
-    <div class="btn-y">
-      <a href="{{ route('admin.plates.index') }}"><button type="button" class="btn btn-warning btn-lg">Torna indietro</button></a>   
+    <div class="container">
+        <form action="{{ route('admin.plates.store') }}" method="POST" id="form" enctype="multipart/form-data">
+
+            @csrf
+
+            <div class="form-group">
+                <label for="img">Foto del piatto</label>
+
+                <div class="custom-file">
+                    <input type="file" name="img"
+                        class="custom-file-input  input-control @error('img')is-invalid @enderror" id="img">
+                    <div class="error"></div>
+
+                    <label class="custom-file-label" for="img">Scegli un file</label>
+                    @error('img')
+                        <div id="img" class="invalid-feedback">
+                            {{ $message }}
+                        </div>
+                    @enderror
+                </div>
+
+            </div>
+
+            <div class="form-group">
+                <label for="name">Nome del piatto</label>
+
+                <input type="text" class="form-control input-control @error('name')is-invalid @enderror" id="name_1"
+                    value="{{ old('name') }}" name="name" autocomplete="name" autofocus aria-describedby="helpName">
+                <div class="error"></div>
+                @error('name')
+                    <div id="name" class="invalid-feedback" role="alert">
+                        {{ $message }}
+                    </div>
+                @enderror
+
+            </div>
+
+            <div class="form-group">
+                <label for="description">Descrizione</label>
+                <textarea class="form-control input-control @error('description')is-invalid @enderror" id="description"
+                    name="description" rows="5">{{ old('description') }}</textarea>
+                <div class="error"></div>
+                @error('description')
+                    <div id="title" class="invalid-feedback">
+                        {{ $message }}
+                    </div>
+                @enderror
+            </div>
+
+            <div class="form-group">
+                <label for="price">Prezzo €</label>
+                <input type="text" class="form-control input-control @error('price')is-invalid @enderror" id="price"
+                    value="{{ old('price') }}" name="price" aria-describedby="price">
+                <div class="error"></div>
+                @error('price')
+                    <div id="price" class="invalid-feedback">
+                        {{ $message }}
+                    </div>
+                @enderror
+
+            </div>
+
+            <div class="form-group">
+                <label for="is_visible">Disponibilità</label>
+                <select name="is_visible" class="form-control @error('is_visible') is-invalid @enderror" id="is_visible">
+                    <option value="1" default>Disponibile</option>
+                    <option value="0">Non Disponibile</option>
+                </select>
+            </div>
+            <div class="form-group">
+                <button type="submit" class="btn btn-secondary" id="submit">Aggiungi</button>
+            </div>
+        </form>
+
+
+
     </div>
-  </div>
-</div>
-<div class="container">
-  <form action="{{ route('admin.plates.store') }}" method="POST" enctype="multipart/form-data">
-  
-    @csrf
-   
-    <div class="form-group">
-      <label for="img">Foto del piatto</label>
-
-      <div class="custom-file">
-        <input type="file" name="img" class="custom-file-input  @error('img')is-invalid @enderror" id="img">
-        <label class="custom-file-label" for="img">Scegli un file</label>
-        @error('img')
-          <div id="img" class="invalid-feedback">
-            {{ $message }}
-          </div>    
-        @enderror
-      </div>
-      
-    </div>
-
-    <div class="form-group">
-      <label for="name">Nome del piatto</label>
-      <div class="error"></div>
-      <input type="text" class="form-control input-control @error('name')is-invalid @enderror" id="name1" value="{{ old('name') }}" name="name" autocomplete="name" autofocus aria-describedby="helpName">
-      <div class="error"></div>
-      @error('name')
-        <div id="name" class="invalid-feedback" role="alert">
-          {{ $message }}
-        </div>    
-      @enderror
-
-    </div>
-
-    <div class="form-group">
-      <label for="description">Descrizione</label>
-      <textarea class="form-control  @error('description')is-invalid @enderror" id="description" name="description" rows="5">{{ old('description')}}</textarea>
-      @error('description')
-        <div id="title" class="invalid-feedback">
-          {{ $message }}
-        </div>    
-      @enderror
-    </div>
-
-    <div class="form-group">
-      <label for="price">Prezzo €</label>
-      <input type="text" class="form-control @error('price')is-invalid @enderror" id="price" value="{{ old('price') }}" name="price" aria-describedby="price">
-      @error('price')
-        <div id="price" class="invalid-feedback">
-          {{ $message }}
-        </div>    
-      @enderror
-
-    </div>
-
-    <div class="form-group">
-      <label for="is_visible">Disponibilità</label>
-      {{-- <input type="number" name="is_visible" class="form-control @error('is_visible') is-invalid @enderror" id="is_visible" placeholder="Inserisci disponibilità" min="0" max="1" value="" required> --}}
-      <select name="is_visible" class="form-control @error('is_visible') is-invalid @enderror" id="is_visible">
-        <option value="1" default>Disponibile</option>
-        <option value="0">Non Disponibile</option>
-      </select>
-    </div>
-    <div class="form-group">
-      <button type="submit" class="btn btn-secondary" id="submit">Aggiungi</button>
-    </div> 
-  </form>
-
- 
-</div>
 @endsection
 
 <style>
-  @media all and (max-width:576px) {
-    .title {
-      font-size: 1.5rem;
+    @media all and (max-width:576px) {
+        .title {
+            font-size: 1.5rem;
+        }
+
+        .btn {
+            scale: 0.8;
+        }
     }
-    .btn {
-      scale: 0.8;
+
+
+    /* .input-control.success {
+        border-color: green;
     }
-  }
 
+    .input-control.error {
+        border-color: red;
+    } */
 
-  .input-control.success {
-    border-color: green;
-  }
-
-  .input-control.error {
-      border-color: red;
-  }
-
-  .inpunt-control .error {
-      color: red;
-      font-size: 9px;
-      height: 13px;
-      
-  }
-
+    .error {
+        color: red;
+    }
 </style>
 
 
 @section('script-js')
-  <script>
-    console.log('ciao');
-    
-    
-    // submitButton.addEventListener('click', function() {
-    //   form.preventDefault();
+    <script>
+        console.log('ciao');
 
-    //   validateForm();
-    // });
 
-    window.addEventListener('load', () => {
+        // submitButton.addEventListener('click', function() {
+        //   form.preventDefault();
 
-      const form = document.getElementById('form');
-      const namePlate = document.getElementById('name1');
-      console.log(namePlate);
-      const description = document.getElementById('description');
-      
-      const price = document.getElementById('price');
-      const isVisible = document.getElementById('is_visible');
-      const submitButton = document.getElementById('submit');
-      submitButton.addEventListener('click', e => {
-        e.preventDefault();
-        if (validateInputs())
-          form.submit();
-        
-        
+        //   validateForm();
+        // });
 
-        //form.submit();
-      })
-    })
+        window.addEventListener('load', () => {
+            const form = document.getElementById('form');
 
-    const setError = (element, message) => {
-      const inputControl = element.parentElement;
-      const errorDisplay = inputControl.querySelector('.error');
-      
-      errorDisplay.innerText = message;
-      inputControl.classList.add('error');
-      inputControl.classList.remove('success');
-      
-    }
+            // const namePlate = document.getElementById('name_1');
+            // console.log(namePlate);
+            // const description = document.getElementById('description');
 
-    const setSuccess = (element) => {
+            // const price = document.getElementById('price');
+            // const isVisible = document.getElementById('is_visible');
+            const submitButton = document.getElementById('submit');
+            submitButton.addEventListener('click', e => {
+                e.preventDefault();
+                if (validateInputs())
+                    form.submit();
 
-      const inputControl = element.parentElement;
-      const errorDIsplay = inputControl.querySelector('.error');
-      
-      errorInput.innerText = '';
-      inputControl.classList.add('success');
-      inputControl.classList.remove('error');
-      
-    }
 
-    function validateInputs() {
-      
-      let validate = true;
-      
-      const nameValue = namePlate.value.trim();
-      const descriptionValue = description.value.trim();
-      const priceValue = price.value.trim();
-      const priceFloat = parsFloat(priceValue);
-      const isVisibleVlue = isVisible.value.trim();
-      
 
-      if(nameValue === '') {
-        setError(name, 'name is required');
-        validate = false;
-      } else {
-        setSuccess(name);
-      }
+                //form.submit();
+            })
+        })
 
-      if(descriptionValue === '') {
-        setError(description, 'description is required');
-        validate = false;
-      } else {
-        setSuccess(description);
-      }  
+        const setError = (element, message) => {
+            const inputControl = element.parentElement;
+            console.log(inputControl)
+            const errorDisplay = inputControl.querySelector('.error');
+            console.log(errorDisplay)
 
-      if(priceValue === '') {
-        setError(price, 'price is required');
-        validate = false;
-      } else {
-        setSuccess(price);
-      }
+            errorDisplay.innerText = message;
+            inputControl.classList.add('error');
+            inputControl.classList.remove('success');
 
-      return validate;
-    }
-    
-  </script>    
+        }
+
+        const setSuccess = (element) => {
+
+            const inputControl = element.parentElement;
+            const errorDisplay = inputControl.querySelector('.error');
+
+            errorInput.innerText = '';
+            inputControl.classList.add('success');
+            inputControl.classList.remove('error');
+
+        }
+
+        function validateInputs() {
+
+            let validate = true;
+
+
+            const nameHtml = document.getElementById('name_1')
+            const description = document.getElementById('description');
+            const price = document.getElementById('price');
+            const isVisible = document.getElementById('is_visible');
+
+
+
+            // console.log(nameHtml, description, price, isVisible)
+
+
+
+
+            const nameValue = nameHtml.value.trim();
+            const descriptionValue = description.value.trim();
+            const priceValue = price.value.trim();
+            // const priceFloat = parsFloat(priceValue);
+            const isVisibleVlue = isVisible.value.trim();
+
+
+            if (nameValue === '') {
+                setError(nameHtml, 'Campo Obbligatorio');
+                validate = false;
+            } else {
+                setSuccess(name);
+            }
+
+            if (descriptionValue === '') {
+                setError(description, 'Campo Obbligatorio');
+                validate = false;
+            } else {
+                setSuccess(description);
+            }
+
+            if (priceValue === '') {
+                setError(price, 'Campo Obbligatorio');
+                validate = false;
+            } else {
+                setSuccess(price);
+            }
+
+            return validate;
+        }
+    </script>
 @endsection
-
-
-
-
-    
