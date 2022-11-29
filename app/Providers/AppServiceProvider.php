cc11aabd31b48babb481d26dd490a1fe4e8f8dec
giusscos <giu.cosenza08@gmail.com>
2022-11-29 19:07:20 +0100

    minor fix env variables

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Braintree\Gateway;
+use Illuminate\Support\Env;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -27,10 +28,10 @@ class AppServiceProvider extends ServiceProvider
         $this->app->singleton(Gateway::class, function($app){
             return new Gateway(
                 [
-                    'environment' => 'sandbox',
-                    'merchantId' => 'xwp6gv6rjzyd6b39',
-                    'publicKey' => 'vdgx9s2zr52qxcck',
-                    'privateKey' => '6c2cc0f2f2f992950215b13cf9be001c',
+                    'environment' => env('BRAINTREE_ENV'),
+                    'merchantId' => env('BRAINTREE_MERCHANT_ID'),
+                    'publicKey' => env('BRAINTREE_PUBLIC_KEY'),
+                    'privateKey' => env('BRAINTREE_PRIVATE_KEY'),
                 ]
             );
         });
