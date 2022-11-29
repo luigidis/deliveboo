240c933c5d6ecb2d59de34529959be0612014914
FabioLongoni <longoni.fabio90@gmail.com>
2022-11-29 10:46:21 +0100

    add modal plates/index view

--- resources/views/admin/plates/index.blade.php
+++ resources/views/admin/plates/index.blade.php
@@ -49,10 +49,31 @@
                             <form action="{{ route('admin.plates.destroy', $plate) }}" method="POST" class="m-0">
                                 @csrf
                                 @method('DELETE')
-                                <button type="submit"
-                                    class="bg_seco_color c_text_color box_shadow_stroke_small py-1 px-2 m-1 card_button mb-2">
+                                <button type="button"
+                                    class="bg_seco_color c_text_color box_shadow_stroke_small py-1 px-2 m-1 card_button mb-2"
+                                    data-toggle="modal" data-target="#exampleModal">
                                     Elimina piatto
                                 </button>
+                                <!-- Modal -->
+                                <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                    <div class="modal-dialog" role="document">
+                                        <div class="modal-content">
+                                            <div class="modal-header">
+                                                <h5 class="modal-title" id="exampleModalLabel"> {{ $plate->name }} </h5>
+                                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                                    <span aria-hidden="true">&times;</span>
+                                                </button>
+                                            </div>
+                                            <div class="modal-body">
+                                                Vuoi eliminare questo piatto dal tuo ristorante?
+                                            </div>
+                                            <div class="modal-footer">
+                                                <button type="button" class="btn btn-danger" data-dismiss="modal">Annulla</button>
+                                                <button type="submit" class="btn btn-secondary">Elimina</button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
                             </form>
                         </div>
                     </div>
