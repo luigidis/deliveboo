19163475f444d171e205f7f750a81fe44249392c
LuigiDiS <luigi.distefano69@gmail.com>
2022-12-01 17:39:57 +0100

    adding svgs and footer as component

---
+++ resources/js/components/TheFooter.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <div class="container flex">
+            <div>
+                
+            </div>
+            <div>
+
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    setup () {
+        
+
+        return {}
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
