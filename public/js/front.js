abcfeab326f9b0755592d3106c333b27dda8ba87
Ste-cracco <s.cracco@yahoo.it>
2022-12-02 23:07:59 +0100

    Update

--- public/js/front.js
+++ public/js/front.js
@@ -48981,7 +48981,7 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! /Users/giusscos/dev/boolean/progetto_finale/deliveboo/resources/js/front.js */"./resources/js/front.js");
+module.exports = __webpack_require__(/*! C:\Users\Stefano\Documents\Boolean\Laravel\deliverboo\resources\js\front.js */"./resources/js/front.js");
 
 
 /***/ })
